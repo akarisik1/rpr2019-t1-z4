c42be8cc29bd87f84010834b67160223404e3cc3
akarisik1 <akarisik1@etf.unsa.ba>
2019-10-18 14:36:48 +0200

    klasa artikl

--- src/ba/unsa/etf/rpr/Artikl.java
+++ src/ba/unsa/etf/rpr/Artikl.java
@@ -1,4 +1,15 @@
 package ba.unsa.etf.rpr;
 
 public class Artikl {
+    String[] naziv;
+    int kod;
+    String[] cijena;
+   public Artikl(String[] naziv, int kod, String[] cijena){
+        this.naziv=naziv;
+        this.kod=kod;
+        this.cijena=cijena;
+    }
+    public void getNaziv() {return naziv;}
+    public void getKod() {return kod;}
+    public void getCijena() {return cijena;}
 }
